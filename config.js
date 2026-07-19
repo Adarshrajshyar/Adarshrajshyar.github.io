9ab4a3fe5282e8379d49dbfe013b229279b229e6
Adarshrajshyar <adarshraj53015@gmail.com>
2026-07-19 14:50:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,3 +3,47 @@ const EMAILJS_CONFIG = {
   SERVICE_ID: "service_x6aju4h",
   TEMPLATE_ID: "template_r93yuzg"
 };
+// ==========================================
+// Adarsh Raj Shayar
+// CONFIG FILE
+// ==========================================
+
+// EmailJS Configuration
+
+const PUBLIC_KEY = "v5vnTSXnFe2kvgWEM";
+
+const SERVICE_ID = "service_x6aju4h";
+
+const TEMPLATE_ID = "template_r93yuzg";
+
+// Website Information
+
+const WEBSITE_NAME = "Adarsh Raj Shayar";
+
+const WEBSITE_VERSION = "2.0";
+
+const WEBSITE_AUTHOR = "Adarsh Raj";
+
+// Social Links
+
+const INSTAGRAM =
+"https://instagram.com/adarshraj4388";
+
+const EMAIL =
+"adarshraj53015@gmail.com";
+
+// Future Ready
+
+const ENABLE_VISITOR_COUNTER = true;
+
+const ENABLE_RANDOM_SHAYARI = true;
+
+const ENABLE_DARK_MODE = true;
+
+const ENABLE_SEARCH = true;
+
+const ENABLE_FAVORITE = true;
+
+const ENABLE_SHARE = true;
+
+const ENABLE_LIKE = true;
