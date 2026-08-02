6bcc96792cf36435503df815c920e9d18a43d6ff
Adarshrajshyar <adarshraj53015@gmail.com>
2026-08-02 19:35:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -871,6 +871,43 @@ if (menuBtn && nav && overlay) {
 }
 
 /* ==========================================
+   ACTIVE MENU
+========================================== */
+
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll",()=>{
+
+    let current="";
+
+    sections.forEach(section=>{
+
+        const sectionTop=section.offsetTop-120;
+
+        if(window.scrollY>=sectionTop){
+
+            current=section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link=>{
+
+        link.classList.remove("active");
+
+        if(link.getAttribute("href")==="#"+current){
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
+
+/* ==========================================
    END OF SCRIPT
 ========================================== */
 
