bf9d8bb5f697d77aeb45dad0892717d1d7b435e6
Adarshrajshyar <adarshraj53015@gmail.com>
2026-08-16 12:26:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const EMAILJS_PUBLIC_KEY = "v5vnTSXnFe2kvgWEM";
+const EMAILJS_PUBLIC_KEY = "kEJqwQlbZ03jFbMFC"; 
 const EMAILJS_SERVICE_ID = "service_3h6mmz4";
 const EMAILJS_TEMPLATE_ID = "template_r93yuzg";
 
