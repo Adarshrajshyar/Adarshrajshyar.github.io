b59e768e64c7465b2196a31e97dc0d308019eea2
Fh mv bb <adarshraj53015@gmail.com>
2026-08-20 15:17:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,23 @@
+const EMAILJS_PUBLIC_KEY = "kEJqwQlbZ03jFbMFC"; 
+const EMAILJS_SERVICE_ID = "service_3h6mmz4";
+const EMAILJS_TEMPLATE_ID = "template_r93yuzg";
 
+/* Website Config */
+
+const WEBSITE_NAME = "Adarsh Raj Shayar";
+const WEBSITE_VERSION = "v11";
+const WEBSITE_AUTHOR = "Adarsh Raj";
+
+const ADMIN_PASSWORD = "ARS2026";
+
+const CONTACT_EMAIL = "your@email.com";
+
+const GITHUB_URL = "https://github.com/Adarshrajshayar";
+const WEBSITE_URL = "https://adarshrajshayar.github.io/";
+
+const ENABLE_DARK_MODE = true;
+const ENABLE_VISITOR_COUNTER = true;
+const ENABLE_CONTACT_FORM = true;
+const ENABLE_CERTIFICATE = true;
+const ENABLE_STORY = true;
+const ENABLE_SHAYARI = true;
